2650d5e7ac5d5939a3089222776666ad8bbffdb2
Philippa <philippa.claire9@gmail.com>
2020-07-17 14:25:52 +0100

    begin styling and responsiveness but not fix reponsive video

---
+++ src/Components/StyledHeader.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.h1`
+  font-family: 'Montserrat Subrayada', sans-serif;
+`;
+
+export default StyledHeader;
---
+++ src/Components/StyledVideo.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledVideo = styled.section`
+  @media only screen and (max-width: 786px) {
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export default StyledVideo;
